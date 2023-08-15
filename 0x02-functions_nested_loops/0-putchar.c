2f4b011530b79edffc8cc76b8817059d8158e24b
Uduak <eddyukpong@gmail.com>
2023-08-15 03:02:18 -0700

    _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,15 @@
+#include <unistd.h>
+
+/**
+ * main:- Entry point
+ *
+ * Description:- A program that prints _putchar, followed by a new line.
+ *
+ * Return:- Always Success (0)
+ */
+
+int main(void)
+{
+	write(1, "_putchar\n", 9);
+	return (0);
+}
