e787bea4e4751bf34353a011ff9898be4efec629
Uduak <eddyukpong@gmail.com>
2023-08-15 03:53:47 -0700

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * Print_alphabet_x10:- A function that
+ * prints the alphabet 10 times
+ *
+ * Return:- void
+ */
+void	print_alphabet_x10(void)
+{
+	int i;
+	int c;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+		_putchar('\n');
+	}
+}
--- 0x02-functions_nested_loops/README.md
+++ 0x02-functions_nested_loops/README.md
@@ -1,2 +1,3 @@
 Task 0: A program that prints _putchar, followed by a new line.
 Task 1: A function that prints the alphabet, in lowercase, followed by a new line.
+Task 2: A function that prints 10 times the alphabet, in lowercase, followed by a new line.
