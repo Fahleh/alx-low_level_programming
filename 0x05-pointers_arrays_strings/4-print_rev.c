c528ef3e15c219949fa7ab46637d16db4dc6675e
Uduak <eddyukpong@gmail.com>
2023-08-21 04:44:51 -0700

    Task 4

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_rev:- A functio that prints a string in reverse
+ *
+ * @s: The string to be printed
+ */
+
+
+void print_rev(char *str)
+{
+	int i = 0;
+
+	while (str[i] != '\0')
+		i++;
+
+	while (i--)
+		_putchar(str[i]);
+	_putchar('\n');
+}
