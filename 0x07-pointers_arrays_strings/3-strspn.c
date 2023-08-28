98eb674e28227616faf99713b5a910a73b30bf11
Uduak <eddyukpong@gmail.com>
2023-08-28 05:57:01 -0700

    Task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ * _strspn:- A function that gets the length of the prefix
+ *
+ * @s: String to be scanned.
+ * @accept: String containing characters to match.
+ *
+ * Return: the number of bytes
+ */
+
+unsigned int _strspn(char *s, char *accept)
+{
+	int z = 0, x, y;
+
+	for (x = 0; s[x] != '\0'; x++)
+	{
+		if (s[x] != 32)
+		{
+			for (y = 0; accept[y] != '\0'; y++)
+			{
+				if (s[x] == accept[y])
+					z++;
+			}
+		}
+		else
+			return (z);
+	}
+		return (z);
+
+}
--- 0x07-pointers_arrays_strings/README.md
+++ 0x07-pointers_arrays_strings/README.md
@@ -3,3 +3,5 @@ Task 0: A function that fills memory with a constant byte.
 Task 1: A function that copies memory area.
 
 Task 2: A function that locates a character in a string.
+
+TAsk 3: A function that gets the length of a prefix substring.
