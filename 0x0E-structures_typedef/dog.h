ed9abbee61f6c10253cd8858d1978cdad536d821
Uduak <eddyukpong@gmail.com>
2023-09-11 05:30:04 -0700

    Task 3

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,7 +11,7 @@
  * Description: A struct for a dog.
  */
 
-struct dog
+typedef struct dog
 {
 	char *name;
 	float age;
@@ -20,8 +20,6 @@ struct dog
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
-
-typedef struct dog dog_t;
 
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
