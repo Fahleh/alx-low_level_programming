6c113fe360bc7c18d7fc36330ffdfff7d4790292
Uduak <eddyukpong@gmail.com>
2023-09-13 05:16:13 -0700

    Task 3

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -24,8 +24,19 @@ int (*get_op_func(char *s))(int, int)
 
 	int i = 0;
 
-	while (ops[i].op != NULL &&  *(ops[i].op) != *s)
+	/*
+	 * while (ops[i].op != NULL &&  *(ops[i].op) != *s)
 		i++;
-	
+
 	return (ops[i].f);
+	*/
+
+	while (ops[i].op != NULL)
+	{
+		if (*s == *(ops[i].op) && *(s + 1) == '\0')
+			return (ops[i].f);
+		i++;
+	}
+	printf("Error\n");
+	exit(99);
 }
