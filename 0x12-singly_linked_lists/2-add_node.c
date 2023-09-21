c49b27bd0f23ef9e15674dcc5d1e5ea07d43356a
Uduak <eddyukpong@gmail.com>
2023-09-21 06:50:23 -0700

    Task 2

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -26,7 +26,7 @@ list_t *add_node(list_t **head, const char *str)
 	new->str = strdup(str);
 	new->len = len;
 	new->next = (*head);
-	(*head) = new;
+	*head = new;
 
-	return (*head);
+	return (head);
 }
