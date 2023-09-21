8394ae889426e1c9eb8373e0c9dc37284cd8fade
Uduak <eddyukpong@gmail.com>
2023-09-21 07:01:28 -0700

    Task 0

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,7 +11,7 @@
 
 size_t print_list(const list_t *h)
 {
-	int  count = 0;
+	size_t  count = 0;
 
 	while (h)
 	{
@@ -23,9 +23,8 @@ size_t print_list(const list_t *h)
 		{
 		printf("[%d] %s\n", h->len, h->str);
 		}
-
-		h = h->next;
 		count++;
+		h = h->next;
 	}
 	return (count);
 }
