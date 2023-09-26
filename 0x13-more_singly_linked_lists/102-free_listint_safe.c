ed3c0fdc50323687d7afa4f54d7d7c43a091dd20
Uduak <eddyukpong@gmail.com>
2023-09-26 04:58:55 -0700

    Task 13

---
+++ 0x13-more_singly_linked_lists/102-free_listint_safe.c
@@ -0,0 +1,58 @@
+#include "lists.h"
+
+/**
+ * _called - A function that checks if a node has been called
+ *
+ * @node: Pointer to node
+ * @called: List of called nodes
+ * @count: Length
+ *
+ * Return: 1 if is visited and 0 otherwise
+ */
+
+int _called(const listint_t *node, const listint_t **called, int count)
+{
+	int i = 0;
+
+	while (i < count)
+	{
+		if (node == called[i])
+			return (1);
+		i++;
+	}
+
+	return (0);
+}
+
+
+/**
+ * free_listint_safe - A function that frees a list even with loop
+ *
+ * @head: Pointer to head
+ *
+ * Return: number of nodes
+ */
+
+size_t free_listint_safe(listint_t **head)
+{
+	listint_t *temp, *called[1024];
+	int count = 0;
+
+	if (!head)
+		return (-1);
+
+	while (*head)
+	{
+		if (_called(*head, called, count))
+		{
+			*head = NULL;
+			break;
+		}
+
+		called[count++] = *head;
+		temp = (*head)->next;
+		free(*head);
+		*head = temp;
+	}
+	return (count);
+}
