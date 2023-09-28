01b307a9a06b025dc02621d60fdf9275822dd32d
Uduak <eddyukpong@gmail.com>
2023-09-28 06:43:17 -0700

    Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@
 
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-	unsigned long int temp = n ^ m, unsigned int count = 0;
+	unsigned long int temp = n ^ m, count = 0;
 
 	while (temp)
 	{
