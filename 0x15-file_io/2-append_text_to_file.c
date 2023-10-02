12e97ec36853bf10afd12bcb17e8d3a7e067d77a
Uduak <eddyukpong@gmail.com>
2023-10-02 12:27:12 -0700

    Task 2

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -27,9 +27,9 @@ int append_text_to_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	for (len = 0; text_content[len]; len++);
+	for (len = 0; text_content[len]; len++)
+		wrt = write(opn, text_content, len);
 
-	wrt = write(opn, text_content, len);
 	if (wrt == -1)
 	{
 		close(opn);
