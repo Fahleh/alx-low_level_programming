3f35706c0ea52d39bcd84920849ab33916c21926
Uduak <eddyukpong@gmail.com>
2023-10-02 12:30:02 -0700

    Task 1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,7 +26,8 @@ int create_file(const char *filename, char *text_content)
 		return (1);
 	}
 
-	for (len = 0; text_content[len]; len++);
+	for (len = 0; text_content[len]; len++)
+		;
 
 	wrt = write(opn, text_content, len);
 	if (wrt == -1)
